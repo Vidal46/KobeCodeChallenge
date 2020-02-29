58e96a18b3e961c11100d518d266f4999f4e960b
Antônio Vidal <antonio.vidal@southsystem.com.br>
2020-02-29 14:20:27 -0300

    feat(KobeCodeChallenge): Dependencies added to build.gradle

--- app/src/main/java/com/southsystem/kobecodechallenge/MovieActivity.kt
+++ app/src/main/java/com/southsystem/kobecodechallenge/MovieActivity.kt
@@ -3,10 +3,10 @@ package com.southsystem.kobecodechallenge
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MovieActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie)
     }
 }
