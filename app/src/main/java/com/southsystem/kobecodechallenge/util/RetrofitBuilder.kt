8cee1315f83792262210405676a4b8647cb883c3
Antônio Vidal <antonio.vidal@southsystem.com.br>
2020-02-29 17:27:32 -0300

    feat(KobeCodeChallenge): Retrofitbuilder implemented

---
+++ app/src/main/java/com/southsystem/kobecodechallenge/util/RetrofitBuilder.kt
@@ -0,0 +1,29 @@
+package com.southsystem.kobecodechallenge.util
+
+import com.google.gson.GsonBuilder
+import com.southsystem.kobecodechallenge.BuildConfig
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+private const val TIMEOUT: Long = 60
+
+fun <T> getApi(serviceClass: Class<T>): T {
+
+    val client = OkHttpClient.Builder()
+    client.readTimeout(TIMEOUT, TimeUnit.SECONDS)
+    client.connectTimeout(TIMEOUT, TimeUnit.SECONDS)
+//    client.addInterceptor(QueryParamInterceptor())
+
+    val gson = GsonBuilder()
+        .setLenient()
+        .create()
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BuildConfig.URL_BASE)
+        .client(client.build())
+        .addConverterFactory(GsonConverterFactory.create(gson))
+
+    return retrofit.build().create(serviceClass)
+}
