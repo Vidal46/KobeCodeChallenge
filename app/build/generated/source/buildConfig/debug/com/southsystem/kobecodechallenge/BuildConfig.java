d7ee1057d4735cd87fcc0717df1aff279142031f
Antônio Vidal <antonio.vidal@southsystem.com.br>
2020-02-29 17:31:15 -0300

    feat(KobeCodeChallenge): Auto-generated binary files added

---
+++ app/build/generated/source/buildConfig/debug/com/southsystem/kobecodechallenge/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.southsystem.kobecodechallenge;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.southsystem.kobecodechallenge";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from build type: debug
+  public static final String URL_BASE = "https://api.themoviedb.org/3/";
+}
