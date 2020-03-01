f05a5614bafadcc901ac704764a52b3115691b4d
Antônio Vidal <antonio.vidal@southsystem.com.br>
2020-03-01 16:05:08 -0300

    feat(KobeCodeChallenge): Util kotlin extensions implemented

---
+++ app/src/main/java/com/southsystem/kobecodechallenge/extension/ImageViewExtensions.kt
@@ -0,0 +1,19 @@
+    package br.com.marvel.extensions
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+
+@BindingAdapter("imageUrl")
+fun ImageView.setImageUrl(url: String) {
+    val circleProgressDrawable = CircularProgressDrawable(context)
+    circleProgressDrawable.strokeWidth = 10f
+    circleProgressDrawable.centerRadius = 50f
+    circleProgressDrawable.start()
+
+    Glide.with(context)
+        .load(url)
+        .placeholder(circleProgressDrawable)
+        .into(this)
+}
