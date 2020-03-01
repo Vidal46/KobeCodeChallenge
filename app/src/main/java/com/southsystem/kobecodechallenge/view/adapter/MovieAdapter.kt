d02543182c3763ca8b4f16f7ce33b5c937be4de1
Antônio Vidal <antonio.vidal@southsystem.com.br>
2020-03-01 16:41:50 -0300

    feat(KobeCodeChallenge): Movies adapter implemented

---
+++ app/src/main/java/com/southsystem/kobecodechallenge/view/adapter/MovieAdapter.kt
@@ -0,0 +1,43 @@
+package com.southsystem.kobecodechallenge.view.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.southsystem.kobecodechallenge.R
+import com.southsystem.kobecodechallenge.databinding.MovieListItemBinding
+import com.southsystem.kobecodechallenge.movie.model.Movie
+
+class MoviesAdapter(
+    private var moviesList: List<Movie>,
+    private val listener: (Movie) -> Unit
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun getItemCount(): Int = moviesList.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding: MovieListItemBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.movie_list_item, parent, false
+        )
+
+        return MovieItemViewHolder(binding, listener)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        holder as MovieItemViewHolder
+        holder.onBind(moviesList[position])
+    }
+
+    class MovieItemViewHolder(
+        private val binding: MovieListItemBinding,
+        private val listener: (Movie) -> Unit
+    ) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun onBind(item: Movie) {
+            binding.movie = item
+            itemView.setOnClickListener { listener(item) }
+            binding.executePendingBindings()
+        }
+    }
+}
