ffa6ffadab527f8ac8f631abf61a0da6efa1a191
Antônio Vidal <antonio.vidal@southsystem.com.br>
2020-03-02 02:48:40 -0300

    fix(KobeCodeChallenge): Imageview extension adjustment

--- app/src/main/java/com/southsystem/kobecodechallenge/extension/ImageViewExtensions.kt
+++ app/src/main/java/com/southsystem/kobecodechallenge/extension/ImageViewExtensions.kt
@@ -1,19 +1,17 @@
-    package br.com.marvel.extensions
+package br.com.marvel.extensions
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+//import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
+import com.southsystem.kobecodechallenge.BuildConfig
+import com.southsystem.kobecodechallenge.R
 
 @BindingAdapter("imageUrl")
-fun ImageView.setImageUrl(url: String) {
-    val circleProgressDrawable = CircularProgressDrawable(context)
-    circleProgressDrawable.strokeWidth = 10f
-    circleProgressDrawable.centerRadius = 50f
-    circleProgressDrawable.start()
+fun ImageView.setImageUrl(url: String?) {
 
     Glide.with(context)
-        .load(url)
-        .placeholder(circleProgressDrawable)
+        .load("${BuildConfig.IMAGES_URL}$url")
+        .placeholder(R.color.colorAccent)
         .into(this)
 }
