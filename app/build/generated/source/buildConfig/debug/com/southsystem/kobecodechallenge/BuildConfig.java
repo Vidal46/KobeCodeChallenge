6b7d1127a14434e813c20feba182a2454ff14509
Antônio Vidal <antonio.vidal@southsystem.com.br>
2020-03-02 02:50:01 -0300

    feat(KobeCodeChallenge): Image url key added

--- app/build/generated/source/buildConfig/debug/com/southsystem/kobecodechallenge/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/southsystem/kobecodechallenge/BuildConfig.java
@@ -11,5 +11,7 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
   // Fields from build type: debug
+  public static final String API_KEY = "c5850ed73901b8d268d0898a8a9d8bff";
+  public static final String IMAGES_URL = "https://image.tmdb.org/t/p/w500";
   public static final String URL_BASE = "https://api.themoviedb.org/3/";
 }
