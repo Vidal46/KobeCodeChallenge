9248dbcba0355315f20ec5756234178c12c4b024
Antônio Vidal <antonio.vidal@southsystem.com.br>
2020-03-02 02:39:10 -0300

    feat(KobeCodeChallenge): Movies adapter filter method implemented

--- app/src/main/java/com/southsystem/kobecodechallenge/view/adapter/MovieAdapter.kt
+++ app/src/main/java/com/southsystem/kobecodechallenge/view/adapter/MovieAdapter.kt
@@ -2,6 +2,8 @@ package com.southsystem.kobecodechallenge.view.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.southsystem.kobecodechallenge.R
@@ -11,9 +13,15 @@ import com.southsystem.kobecodechallenge.movie.model.Movie
 class MoviesAdapter(
     private var moviesList: List<Movie>,
     private val listener: (Movie) -> Unit
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>(), Filterable {
 
     override fun getItemCount(): Int = moviesList.size
+
+    var filteredMovies = mutableListOf<Movie>()
+
+    init {
+        filteredMovies.addAll(moviesList)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val binding: MovieListItemBinding = DataBindingUtil.inflate(
@@ -22,6 +30,38 @@ class MoviesAdapter(
         )
 
         return MovieItemViewHolder(binding, listener)
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(charSequence: CharSequence): FilterResults {
+                val charString = charSequence.toString()
+                val newMoviesList = mutableListOf<Movie>()
+
+                if (charString.isNotEmpty() && charSequence.length >= 3) {
+                    moviesList.forEach {
+                        if (it.name?.contains(charString, true)!!) {
+                            newMoviesList.add(it)
+                        }
+                    }
+                } else {
+                    newMoviesList.addAll(moviesList)
+                }
+
+                val filterResults = FilterResults()
+
+                filterResults.run {
+                    values = newMoviesList
+                    count = newMoviesList.size
+                }
+                return filterResults
+            }
+
+            override fun publishResults(charSequence: CharSequence, filterResults: FilterResults) {
+                filteredMovies = filterResults.values as MutableList<Movie>
+                notifyDataSetChanged()
+            }
+        }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
